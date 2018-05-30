6ff76d2fbcc4c3c5cc62705831ad0f07647afc5f
tianyao <>
2018-05-30 19:59:15 +0800

    基础

---
+++ SwiftLanguage.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// 常量和变量
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.1
+
+// 类型注释
+var welcomeMessage: String
+welcomeMessage = "Hello"
+var red, green, blue: Double
+
+// 命名常量和变量
+let 你好 = "你好世界"
+let 🐶🐷 = "god"
+let `let` = "god"
+
+// 打印常量和变量
+print("welcomeMessage is \(str)")
+
+// 整数界限
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+// Int32    32位平台
+// Int64    64位平台
+// UInt     32位平台：UInt32, 64位平台：UInt64
+// Double   64位浮点数
+// Float    32位浮点数
+
+// 类型安全和类型推断
+let meaningOfLife = 42      // Int
+let pi = 3.14159            // Double
+let anotherPi = 3 + 0.14159 // Double
+
+// 数字文字
+let decimalInteger = 19         // 十进制
+let binaryInteger = 0b10001     // 二进制
+let octalInteger = 0o21         // 八进制
+let hexadecimalInteger = 0x11   // 十六进制
+
+
+
+
